d4de36cd2c054eaad7e75ad28e92598e90d1c9e7
ran1004 <phr610600@163.com>
2019-12-10 21:34:18 -0800

    Bin

---
+++ sort/Bin_Sort.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void Bin_Sort(int A[], int N) {
+	if (N == 1)
+		return;
+}
+
+int main() {
+/*	int A[];	 
+	int N = sizeof(A)/sizeof(A[0]);
+	Bin_Sort(A, N)
+	int i;
+	for (i=0; i<N; i++)
+		printf("%3d", A[i]);
+	printf("\n"); */
+}
